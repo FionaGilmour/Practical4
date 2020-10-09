e0c86000975525d9a328608f060b3b2d1dff9367
Gilmour, Fiona <40315836@live.napier.ac.uk>
2020-10-09 15:59:57 +0100

    Testing...

--- Practical4/Program.cs
+++ Practical4/Program.cs
@@ -11,6 +11,7 @@ namespace Practical4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing... Testing...");
         }
     }
 }
